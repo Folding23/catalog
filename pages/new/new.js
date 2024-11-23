1f2ff01f3734a69b2f8e93f7eb9b8d2342de8fae
Folding23 <1658223867@qq.com>
2024-11-23 11:32:00 +0800

    food first commit

--- pages/new/new.js
+++ pages/new/new.js
@@ -5,14 +5,16 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        multiArray: [['教学区', '生活区'], ['西三教学楼', '西二教学楼', '西一教学楼', '中楼', '东三教学楼'], ['一楼', '二楼','三楼']],
+        multiIndex: [0, 0, 0],
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        
+        this.shot()
     },
 
     /**
@@ -21,6 +23,77 @@ Page({
     onReady() {
 
     },
+
+    bindMultiPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+          multiIndex: e.detail.value
+        })
+    },
+
+    bindMultiPickerColumnChange: function (e) {
+        console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+        var data = {
+          multiArray: this.data.multiArray,
+          multiIndex: this.data.multiIndex
+        };
+        data.multiIndex[e.detail.column] = e.detail.value;
+        switch (e.detail.column) {
+          case 0:
+            switch (data.multiIndex[0]) {
+              case 0:
+                data.multiArray[1] = ['西三教学楼', '西二教学楼', '西一教学楼', '中楼', '东三教学楼'];
+                data.multiArray[2] = ['一楼', '二楼','三楼'];
+                break;
+              case 1:
+                data.multiArray[1] = ['一区', '二区', '三区'];
+                data.multiArray[2] = ['宿舍楼', '食堂'];
+                break;
+            }
+            data.multiIndex[1] = 0;
+            data.multiIndex[2] = 0;
+            break;
+          case 1:
+            switch (data.multiIndex[0]) {
+              case 0:
+                switch (data.multiIndex[1]) {
+                  case 0:
+                    data.multiArray[2] = ['一楼', '二楼','三楼'];
+                    break;
+                  case 1:
+                    data.multiArray[2] = ['一楼', '二楼','三楼'];
+                    break;
+                  case 2:
+                    data.multiArray[2] = ['一楼', '二楼','三楼'];
+                    break;
+                  case 3:
+                    data.multiArray[2] = ['一楼', '二楼','三楼'];
+                    break;
+                  case 4:
+                    data.multiArray[2] = ['一楼', '二楼','三楼'];
+                    break;
+                }
+                break;
+              case 1:
+                switch (data.multiIndex[1]) {
+                  case 0:
+                    data.multiArray[2] = ['宿舍楼', '食堂'];
+                    break;
+                  case 1:
+                    data.multiArray[2] = ['宿舍楼', '食堂'];
+                    break;
+                  case 2:
+                    data.multiArray[2] = ['宿舍楼', '食堂'];
+                    break;
+                }
+                break;
+            }
+            data.multiIndex[2] = 0;
+            console.log(data.multiIndex);
+            break;
+        }
+        this.setData(data);
+      },
 
     /**
      * 生命周期函数--监听页面显示
@@ -62,5 +135,21 @@ Page({
      */
     onShareAppMessage() {
 
+    },
+    shot() {
+        
+        const ctx = wx.createCameraContext()
+        console.log("hhh")
+        ctx.takePhoto({
+            quality: 'high',
+            success:(res) => {
+                console.log('sucess !!!!!')
+                console.log(res)
+            },
+            fail: (res)=> {
+                console.log("fail!!!")
+            }
+        })
+        console.log("hhh")
     }
 })
