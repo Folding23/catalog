41a7dbb46b1c54c28475613f9a38146e2cd53ca3
Folding23 <1658223867@qq.com>
2024-11-25 19:37:31 +0800

    food first commit

--- pages/live/live.js
+++ pages/live/live.js
@@ -12,6 +12,24 @@ Page({
             'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
         ],
         videos:[],
+        info:[{
+            name:"直播间1",
+            intro:"1区1号楼"
+        },{
+            name:"直播间2",
+            intro:"2区2号楼"
+        },{
+            name:"直播间3",
+            intro:"3区3号楼"
+        },{
+            name:"直播间4",
+            intro:"4区4号楼"
+        },{
+            name:"直播间5",
+            intro:"5区5号楼"
+        }],
+        name:"",
+        intro:""
     },
 
     /**
@@ -21,9 +39,20 @@ Page({
         let videos = this.data.urls.map((url, index) => ({ id: index + 1, url, objectFit:"contain" }));
         this.setData({
             videos,
+            name:this.data.info[0].name,
+            intro:this.data.info[0].intro,
         })
     },
 
+    // 切换直播间
+    onChange(e){
+        console.log(e.detail);
+        let index = e.detail.activeId;
+        this.setData({
+            name:this.data.info[index-1].name,
+            intro:this.data.info[index-1].intro,
+        })
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
