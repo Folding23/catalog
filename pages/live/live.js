852319d184be94c3127566242b492dd72e5dc322
Folding23 <1658223867@qq.com>
2024-11-27 10:50:05 +0800

    food first commit

--- pages/live/live.js
+++ pages/live/live.js
@@ -8,8 +8,6 @@ Page({
             'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
             'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
             'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
-            'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
-            'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
         ],
         videos:[],
         info:[{
@@ -21,15 +19,8 @@ Page({
         },{
             name:"直播间3",
             intro:"3区3号楼"
-        },{
-            name:"直播间4",
-            intro:"4区4号楼"
-        },{
-            name:"直播间5",
-            intro:"5区5号楼"
-        },],
-        name:"",
-        intro:""
+        }],
+        index:1,
     },
 
     /**
@@ -39,19 +30,20 @@ Page({
         let videos = this.data.urls.map((url, index) => ({ id: index + 1, url, objectFit:"contain" }));
         this.setData({
             videos,
-            name:this.data.info[0].name,
-            intro:this.data.info[0].intro,
         })
     },
 
-    // 切换直播间
+    // 切换直播间时，同时切换简介
     onChange(e){
         console.log(e.detail.activeId);
-        let index = e.detail.activeId;
+        let index = e.detail.activeId-1;
         this.setData({
-            name:this.data.info[index-1].name,
-            intro:this.data.info[index-1].intro,
+            index,
         })
+    },
+    // 点击按钮切换直播间
+    switch(){
+
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
